1e0337caf5fd7d866b9bec169eae47aae39b925d
jesus2102 <jesusperezmarin@gmail.com>
2018-07-10 16:07:26 +0100

    classes' setup done

--- src/main/java/Models/File.java
+++ src/main/java/Models/File.java
@@ -1,7 +1,13 @@
 package Models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class File {
 
+    private int id;
     private String name;
     private String extension;
     private double size;
@@ -12,4 +18,41 @@ public class File {
         this.size = size;
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "extension")
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    @Column(name = size)
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
 }
--- src/main/java/Models/Folder.java
+++ src/main/java/Models/Folder.java
@@ -1,10 +1,36 @@
 package Models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Folder {
 
+    private int id;
     private String title;
 
+    public Folder(String title){
+        this.title = title;
+    }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
