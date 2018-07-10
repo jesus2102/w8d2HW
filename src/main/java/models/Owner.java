dabb4aafa15d6195e9205473a06f097edb88f82c
jesus2102 <jesusperezmarin@gmail.com>
2018-07-10 20:46:11 +0100

    Owner setup done

--- src/main/java/models/Owner.java
+++ src/main/java/models/Owner.java
@@ -1,4 +1,49 @@
 package models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Owners")
 public class Owner {
+
+    private int id;
+    private String name;
+    private String userName;
+
+    public Owner(){}
+
+    public Owner(String name, String userName){
+        this.name = name;
+        this.userName = userName;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "user_name")
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
